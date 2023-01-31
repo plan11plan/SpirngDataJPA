9e5fb50dcb86efda42ebaeaf7af86a52499b1ba7
plan11plan <pmsuk7028@naver.com>
2023-01-31 16:55:20 +0900

    .

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -19,27 +19,33 @@ public interface MemberRepository extends JpaRepository<Member,Long>,MemberRepos
 //    @Query(name="Member.findByUsername")
     List<Member> findByUsername(@Param("username")String username);
 
-    @Query("select m from Member m where m.username = :username and m.age = :age")
-    List<Member> findUser(@Param("username")String username,@Param("age")int age);
+
+    @Query("select m from Member m where m.username=:username and m.age=:age")
+    List<Member> findUser (@Param("username") String username,@Param("age") int age);
 
     @Query("select m.username from Member m")
     List<String> findUsernameList();
-    //DTO로 조회할때는 무조건 New Operation을 써야합니다.
+    /**
+     * @Query로 DTO 조회하기
+     * DTO로 조회할때는 무조건 New Operation을 써야합니다.
+    */
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
             "from Member m join m.team t")
     List<MemberDto> findMemberDto();
 
     @Query("select m from Member m where m.username in :names")
-    List<Member> findByNames(@Param("names") List<String> names);
+    List<Member> findByNames(@Param("names") List<String>names);
 
     List<Member> findListByUsername(String username); //컬렉션
     Member findMemberByUsername(String username);//단건
     Optional<Member> findOptionByUsername(String username); //단건 Optional
 
-
-//    @Query(value = "select m from Member m",
-//            countQuery ="select count(m) from Member m")
-    Page<Member> findByAge(int age, Pageable pageable);
+    /**
+     * 페이징이 들어간 스프링 데이터 JPA
+     */
+//    @Query(value = "select m from Member m", 
+//            countQuery ="select count(m) from Member m")  --> 카운트만 가져올때 쓰는 쿼리(성능 최적화)
+    Page<Member> findByAge(int age, Pageable pageable);  // Pageable 인터페이스(쿼리 조건 들어감) 넘김.
 
     @Modifying(clearAutomatically = true)
     @Query("update Member m set m.age = m.age+1 where m.age>=:age")
